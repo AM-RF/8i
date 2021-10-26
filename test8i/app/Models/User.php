af83ecf8be2a56d2aef6b8f8c9533cffd27f014b
AM-RF <alonso19rojas@gmail.com>
2021-10-26 00:11:48 -0400

    feat (relationship) create relationship

--- test8i/app/Models/User.php
+++ test8i/app/Models/User.php
@@ -41,4 +41,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relacion uno a uno
+    public function profile() {
+        return $this->hasOne('App\Models\Profile');
+    }
+
+    // Relacion uno a muchos
+    public function posts() {
+        return $this->hasMany('App\Models\Post');
+    }
+
+    // Relacion muchos a muchos
+    public function roles() {
+        return $this->belongsToMany('App\Models\Role');
+    }
 }
