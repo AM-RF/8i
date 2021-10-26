3bfe32859ac424d7a9754eacbbb4906a94192081
AM-RF <alonso19rojas@gmail.com>
2021-10-26 11:52:37 -0400

    feat: email queue y job

--- test8i/app/Http/Controllers/homeController.php
+++ test8i/app/Http/Controllers/homeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+use App\Jobs\SendEmail;
+
 
 class HomeController extends Controller
 {
@@ -26,6 +29,8 @@ class HomeController extends Controller
     {
         $response = Http::get('https://jsonplaceholder.typicode.com/todos/');
         $response = $response->json();
+        //SendEmail::dispatch('danniriesthefany96@gmail.com', $response[0]['title']); EJEMPLO
+        //SendEmail::dispatch('emailaqui', $response[0]['title']);
         return view('home', compact('response'));
     }
 }
